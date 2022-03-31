ac6849a026e1319792cfe8cc8ab9f66383632a9e
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-03-31 22:08:46 +0530

    Add files via upload

---
+++ josh.py
@@ -0,0 +1,22 @@
+# print("hello",end=' ' "w") 
+# print("hello",end='-' "w") 
+# print("hello",end=' .' "w") #hello whello-whello.whello w
+
+# print("hello",end=' ' "w") #hello w
+# print("\nhello",end=' -' "w") #hello -w
+# print("\nhello",end=' ,' "w") #hello ,w
+# print("hello",end=''"welcome to josh innovations\n") #hellowelcome to josh innovations
+# print("python programming",end=",")  
+# print("from josh innovation",end=".")  
+# print("course: core python programming") #python programming,from josh innovation. course:core python programming
+# print("hello ipl on 26th march","2days to go",sep='-') #hello ipl on 26th march-2days to go 
+# print("hello",sep=".""josh innovation") #hello
+# print("core","python","programming",sep="*") #core*python*programming
+print("ameerpet metro station",end=".") #ameerpet metro station.near
+print("near sap street lane",end="\n")
+print("josh","innovations",end='.',sep="-") 
+
+
+
+
+ 
